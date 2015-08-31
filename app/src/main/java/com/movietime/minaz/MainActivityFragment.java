353a203dbdf880aed13ac93b7d6d680bff4387a6
Mina S. Aziz <minaz922@gmail.com>
2015-08-31 23:04:10 +0200

    Initialize Project

---
+++ app/src/main/java/com/movietime/minaz/MainActivityFragment.java
@@ -0,0 +1,25 @@
+package com.movietime.minaz;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainActivityFragment extends Fragment {
+    public MainActivityFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+}
